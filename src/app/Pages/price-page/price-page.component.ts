e6d74bca5a38ed886b408a18a1c2dcf388d45348
Lua19 <miguellua100@gmail.com>
2022-02-28 16:30:49 -0700

    Added input fields

--- src/app/Pages/price-page/price-page.component.ts
+++ src/app/Pages/price-page/price-page.component.ts
@@ -8,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class PricePageComponent implements OnInit {
   Fee : number = 0;
   CreditCardFee = 0;
-  Money: number =0;
+  Earnings : number[] = [];
+
+  public machines : any [] =
+  [
+    {name: '',
+     earnings: 0}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  addMachine() {
+    this.machines.push(
+      {name: '',
+      earnings: 0}
+      );
+    }
+  removeMachine(i: number) {
+    this.machines.splice(i, 1);
+  }
+
+  calculateFee(Fee : number){
+    
+    if (Fee>0) {
+      
+    }
   }
 
 }
