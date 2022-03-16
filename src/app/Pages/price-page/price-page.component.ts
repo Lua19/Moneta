d1cf6c15f0bb7d9c371e9663d72d15319f08a1fd
Lua19 <miguellua100@gmail.com>
2022-03-16 14:39:49 -0700

    Added monthly fees

--- src/app/Pages/price-page/price-page.component.ts
+++ src/app/Pages/price-page/price-page.component.ts
@@ -12,58 +12,64 @@ export class PricePageComponent implements OnInit {
   creditFee : number = 0;
 
   machines : number[] = [0];
-  cardSales : number[] = [0];
+  singleFee : number[] = [0];
+  sales : any[] = [0];
 
   constructor() { }
 
   ngOnInit(): void {
     this.machines = [0];
 
-    this.cardSales = [0];
+    this.sales = [0];
+    this.singleFee = [0];
   }
 
   addMachine(){
     this.machines.push(0)
 
-    this.cardSales.push(0)
+    this.sales.push(0)
+    this.singleFee.push(0)
   }
   removeMachine(i : number){
     this.machines.splice(i,1);
 
-    this.cardSales.splice(i,1);
+    this.sales.splice(i,1);
+    this.singleFee.splice(i,1);
+    this.calcFee();
   }
 
   /*
   cardFee(){
     this.creditFee = 0;
     for (let index = 0; index < this.machines.length; index++) {
-      this.creditFee += this.cardSales[index];
+      this.creditFee += this.sales[index];
     }
     this.creditFee = this.creditFee * 0.051;
     return this.creditFee;
   }
   */
 
-  calcFee(myform: NgForm){
+  calcFee(){
     this.monthlyFee = 0;
     this.totalFee = 0;
 
 
     for (let index = 0; index < this.machines.length; index++) {
-      this.totalFee += this.cardSales[index];
+      this.totalFee += this.sales[index];
 
       if (this.totalFee > 0 && this.totalFee <= 2999) {
-        this.monthlyFee = this.monthlyFee + 50; 
+        this.monthlyFee = this.monthlyFee + 50;
+        this.singleFee[index] = 50;
       }
       if (this.totalFee >= 3000 && this.totalFee <= 4999) {
-        this.monthlyFee = this.monthlyFee + 75;        
+        this.monthlyFee = this.monthlyFee + 75;
+        this.singleFee[index] = 75;        
       }
       if (this.totalFee >= 5000) {
-        this.monthlyFee = this.monthlyFee + 100;        
+        this.monthlyFee = this.monthlyFee + 100;
+        this.singleFee[index] = 100;
       }
       this.totalFee = 0;
     }
-    
-    console.log(this.monthlyFee);
   }
 }
