05fa506001e91b5b8285c8c5af564f7cabdc37ec
Lua19 <miguellua100@gmail.com>
2022-04-06 16:24:26 -0600

    Dashboard changed to admins only

--- src/app/Pages/store-page/store-page.component.ts
+++ src/app/Pages/store-page/store-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/Interfaces/Product.interface';
+import { WebUser } from 'src/app/Interfaces/WebUser.interface';
 import { AuthService } from 'src/app/Services/auth.service';
 import { ProductsService } from 'src/app/Services/products.service';
 
@@ -12,17 +13,21 @@ import { ProductsService } from 'src/app/Services/products.service';
 export class StorePageComponent implements OnInit {
 
   productsList: Product[] = [];
-  isUserLoggedIn: boolean = false
+  isUserLoggedIn: boolean = false;
+
+  user:WebUser|any
 
   constructor(private router : Router, private products: ProductsService, private auth : AuthService) {
     this.auth.isUserLoggedIn.subscribe( value => {
       this.isUserLoggedIn = value;
-  });
+    });
    }
 
   ngOnInit(): void {
     this.products.getProducts().subscribe(
       (res) => this.productsList = res);
+
+    this.user = this.auth.user;
   }
 
   navigate(route: string){
