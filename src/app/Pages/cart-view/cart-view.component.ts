7207e75a8b68879af77eba56427df5d22430b0a2
Lua19 <miguellua100@gmail.com>
2022-04-18 13:10:35 -0600

    Added cart view and user name

--- src/app/Pages/cart-view/cart-view.component.ts
+++ src/app/Pages/cart-view/cart-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/Interfaces/Product.interface';
+import { WebUser } from 'src/app/Interfaces/WebUser.interface';
 import { AuthService } from 'src/app/Services/auth.service';
 import { ProductsService } from 'src/app/Services/products.service';
 
@@ -10,15 +11,18 @@ import { ProductsService } from 'src/app/Services/products.service';
 })
 export class CartViewComponent implements OnInit {
   productsList: Product[] = [];
+  user : WebUser | any;
   loading : boolean = true;
   constructor(private products:ProductsService, private auth:AuthService) {
     
    }
 
   ngOnInit(): void {
-    this.productsList = this.products.productsInCart
+    this.productsList = this.products.productsInCart;
+    this.user = this.auth.user;
+    
   }
 
-  
+
 
 }
