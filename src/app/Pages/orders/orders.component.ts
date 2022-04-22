95f5e45886cf089f303ee4c66e99728068fb14c8
Lua19 <miguellua100@gmail.com>
2022-04-22 12:09:04 -0600

    Order page now uses url params

--- src/app/Pages/orders/orders.component.ts
+++ src/app/Pages/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Order } from 'src/app/Interfaces/Order.interface';
 import { Product } from 'src/app/Interfaces/Product.interface';
 import { WebUser } from 'src/app/Interfaces/WebUser.interface';
@@ -14,24 +15,34 @@ export class OrdersComponent implements OnInit {
   Order : any;
   user : WebUser|any;
   productsList : Product[] = []
-  constructor(private auth: AuthService, private products: ProductsService) { }
+
+  // myUrl= new URL('https://www.monetamarket.com/orders?BillingAddress=1246 E WOOD AVE&BillingCity=Salt Lake city&billingfirstname=luis&billinglastname=ruiz&billingstate=utah&billingzip=84105&amount=0.30&cardexpirationdate=0624&cardtype=48**********6705&lasttransactiontype=sale&maskedcardnumber=48**********6705&phone=4322143128&email=luisruiz.latic@gmail.com');
+
+  // obj = new URLSearchParams(this.myUrl.search)
+
+  constructor(private auth: AuthService, private products: ProductsService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.user = this.auth.user;
     localStorage.removeItem("Products")
     this.getProductsForOrder();
-    this.getOrder();
+    // this.getOrder();
+    this.route.queryParams.subscribe(
+      params => {
+        this.Order = params
+      }
+    )
   }
 
-  getOrder(){
-    this.Order = JSON.parse(sessionStorage.getItem("Order")!)
-    console.log(this.Order);
+  // getOrder(){
+  //   this.Order = JSON.parse(sessionStorage.getItem("Order")!)
+  //   // console.log(this.Order);
     
-  }
+  // }
 
   getProductsForOrder(){
     this.productsList = JSON.parse(sessionStorage.getItem("Products")!)
-    console.log(this.productsList)
+    // console.log(this.productsList)
   }
 
 }
